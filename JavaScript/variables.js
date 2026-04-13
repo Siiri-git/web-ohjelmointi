4d06da018bd738f65d9b80885c8bb8d89363f7eb
siiri <myllymakisiiri@gmail.com>
2026-04-13 14:28:19 +0300

    changed variables

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -1,19 +1,34 @@
-// number variables
-const a = 3;
-const b = 5;
-const c = 7;
-const d = 15;
-
 // meth buttons
-function plus() {
-    alert(a + b)
+function plus(x, y) {
+    alert(x + y);
 }
-function minus() {
-    alert(b - c)
+function minus(x, y) {
+    alert(x - y);
 }
-function multi() {
-    alert(c * d)
+function multi(x, y) {
+    alert(x * y);
 }
-function div() {
-    alert(d / a)
+function div(x, y) {
+    alert(x / y);
+}
+
+function calc_sum() {
+    var num1 = Number(document.getElementById("nmbr1").value);
+    var num2 = Number(document.getElementById("nmbr2").value);
+    plus(num1, num2)
+}
+function calc_subst() {
+    var num1 = Number(document.getElementById("nmbr1").value);
+    var num2 = Number(document.getElementById("nmbr2").value);
+    minus(num1, num2)
+}
+function calc_times() {
+    var num1 = Number(document.getElementById("nmbr1").value);
+    var num2 = Number(document.getElementById("nmbr2").value);
+    multi(num1, num2)
+}
+function calc_div() {
+    var num1 = Number(document.getElementById("nmbr1").value);
+    var num2 = Number(document.getElementById("nmbr2").value);
+    div(num1, num2)
 }
