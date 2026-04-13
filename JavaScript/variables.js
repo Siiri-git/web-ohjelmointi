ba96d214d4d849336c4f22f8f37fdba56e7a85c8
siiri <myllymakisiiri@gmail.com>
2026-04-13 12:23:37 +0300

    added variables page

---
+++ JavaScript/variables.js
@@ -0,0 +1,19 @@
+// number variables
+const a = 3;
+const b = 5;
+const c = 7;
+const d = 15;
+
+// meth buttons
+function plus() {
+    alert(a + b)
+}
+function minus() {
+    alert(b - c)
+}
+function multi() {
+    alert(c * d)
+}
+function div() {
+    alert(d / a)
+}
