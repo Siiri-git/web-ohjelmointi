25304c3b56c89fd7ef3b43da3a904c58f0bf0213
siiri <myllymakisiiri@gmail.com>
2026-04-27 14:18:35 +0300

    added date time button

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -33,3 +33,27 @@ function calc_div() {
     div(num1, num2)
 }
 
+function dateTime() {
+    const newDate = new Date();
+
+    const weekdayList = ["Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Tostai", "Perjantai", "Lauantai"];
+    const monthList = ["Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kesäkuu", "Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"];
+
+    const nDate = newDate.toLocaleDateString();
+    const nTime = newDate.toLocaleTimeString();
+    const nWeekday = newDate.getDay();
+    const nMonth = newDate.getMonth();
+
+    var date = document.getElementById("date");
+    var time = document.getElementById("time");
+    var dateAndTime = document.getElementById("dateAndTime");
+    var weekday = document.getElementById("weekday");
+    var month = document.getElementById("month");
+
+    date.innerHTML = `Päivämäärä: ${nDate}`;
+    time.innerHTML = `Päivämäärä: ${nTime}`;
+    dateAndTime.innerHTML = `Päivämäärä & kellonaika: ${nDate} klo ${nTime}`;
+    weekday.innerHTML = `Viikonpäivä: ${weekdayList[nWeekday]}`
+    month.innerHTML = `Kuukausi: ${monthList[nMonth]}`
+
+}
