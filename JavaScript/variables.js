77b66d38e812c28263a74e096f3dfd57b80ae46f
siiri <myllymakisiiri@gmail.com>
2026-04-30 12:45:13 +0300

    lisää henkilöitä lisätty

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -1,4 +1,5 @@
-// meth buttons
+// Math section
+  // buttons
 function plus(x, y) {
     alert(x + y);
 }
@@ -11,7 +12,7 @@ function multi(x, y) {
 function div(x, y) {
     alert(x / y);
 }
-
+  // Functions
 function calc_sum() {
     var num1 = Number(document.getElementById("nmbr1").value);
     var num2 = Number(document.getElementById("nmbr2").value);
@@ -33,7 +34,10 @@ function calc_div() {
     div(num1, num2)
 }
 
+
+// Datetime section
 function dateTime() {
+    // variables
     const newDate = new Date();
 
     const weekdayList = ["Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Tostai", "Perjantai", "Lauantai"];
@@ -49,11 +53,76 @@ function dateTime() {
     var dateAndTime = document.getElementById("dateAndTime");
     var weekday = document.getElementById("weekday");
     var month = document.getElementById("month");
-
+    // variable inner html
     date.innerHTML = `Päivämäärä: ${nDate}`;
     time.innerHTML = `Päivämäärä: ${nTime}`;
     dateAndTime.innerHTML = `Päivämäärä & kellonaika: ${nDate} klo ${nTime}`;
     weekday.innerHTML = `Viikonpäivä: ${weekdayList[nWeekday]}`
     month.innerHTML = `Kuukausi: ${monthList[nMonth]}`
 
+}
+
+// Sets section
+const veijoSect = document.getElementById("veijo");
+var veijo = new Set(['luku', 'poisto']);
+for (access of veijo) {
+    var lista = document.createElement("ul");
+    var veijoInfo = document.createElement("li");
+    veijoInfo.innerHTML = access;
+    lista.append(veijoInfo);
+    veijoSect.append(lista);
+};
+
+const elviraSect = document.getElementById("elvira");
+var elvira = new Set(['luku', 'kirjoitus', 'muokkaus']);
+for (access of elvira) {
+    var lista = document.createElement("ul");
+    var elviraInfo = document.createElement("li");
+    elviraInfo.innerHTML = access;
+    lista.append(elviraInfo);
+    elviraSect.append(lista);
+};
+
+const mehdiSect = document.getElementById("mehdi");
+var mehdi = new Set(['luku', 'muokkaus', 'poisto']);
+for (access of mehdi) {
+    var lista = document.createElement("ul");
+    var mehdiInfo = document.createElement("li");
+    mehdiInfo.innerHTML = access;
+    lista.append(mehdiInfo);
+    mehdiSect.append(lista);
+};
+
+const tuuliSect = document.getElementById("tuuli");
+var tuuli = new Set(['kirjoitus', 'poisto']);
+for (access of tuuli) {
+    var lista = document.createElement("ul");
+    var tuuliInfo = document.createElement("li");
+    tuuliInfo.innerHTML = access;
+    lista.append(tuuliInfo);
+    tuuliSect.append(lista);
+};
+
+const veTuSect = document.getElementById("veijotuuli");
+var veTu = veijo.union(tuuli);
+for (access of veTu) {
+    var veTuInfo = document.createElement("li");
+    veTuInfo.innerHTML = access;
+    veTuSect.append(veTuInfo)
+}
+
+const meElSect = document.getElementById("mehdielvira");
+var meEl = mehdi.intersection(elvira);
+for (access of meEl) {
+    var meElInfo = document.createElement("li");
+    meElInfo.innerHTML = access;
+    meElSect.append(meElInfo)
+}
+
+const elTuSect = document.getElementById("elviratuuli");
+var elTu = elvira.symmetricDifference(tuuli);
+for (access of elTu) {
+    var elTuInfo = document.createElement("li");
+    elTuInfo.innerHTML = access;
+    elTuSect.append(elTuInfo)
 }
